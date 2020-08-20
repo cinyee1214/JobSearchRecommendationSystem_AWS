aa2b0d73324e5c3d0d0a3d531c4e176f5dad6f32
cinyee1214 <xinyizhao1214@gmail.com>
2020-08-19 22:53:31 -0400

    Item entity and update GitHubClient

--- src/main/java/com/laioffer/job/external/GitHubClient.java
+++ src/main/java/com/laioffer/job/external/GitHubClient.java
@@ -1,5 +1,7 @@
 package com.laioffer.job.external;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.entity.Item;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
@@ -8,14 +10,18 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class GitHubClient {
     private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";
     private static final String DEFAULT_KEYWORD = "developer";
 
-    public String search(double lat, double lon, String keyword) {
+    public List<Item> search(double lat, double lon, String keyword) {
         if (keyword == null) {
             keyword = DEFAULT_KEYWORD;
         }
@@ -30,15 +36,22 @@ public class GitHubClient {
         CloseableHttpClient httpclient = HttpClients.createDefault();
 
         // Create a custom response handler
-        ResponseHandler<String> responseHandler = response -> {
+        ResponseHandler<List<Item>> responseHandler = response -> {
             if (response.getStatusLine().getStatusCode() != 200) {
-                return "";
+                return Collections.emptyList();
             }
             HttpEntity entity = response.getEntity();
             if (entity == null) {
-                return "";
+                return Collections.emptyList();
             }
-            return EntityUtils.toString(entity);
+
+            ObjectMapper mapper = new ObjectMapper();
+            
+            InputStream inputStream = entity.getContent();
+            Item[] items = mapper.readValue(inputStream, Item[].class);
+            return Arrays.asList(items);
+
+            //return Arrays.asList(mapper.readValue(entity.getContent(), Item[].class));
         };
 
         try {
@@ -46,7 +59,9 @@ public class GitHubClient {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "";
+
+        return Collections.emptyList();
+        // better than return new ArrayList<>();
     }
 
 }
